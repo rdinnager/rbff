5c38db4b55948a2e4db1aee9cd7a52e071254387
rdinnager <r.dinnage@gmail.com>
2021-08-11 15:46:00 -0400

    first commit. package skeleton in.

---
+++ R/rbff-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @useDynLib rbff, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
